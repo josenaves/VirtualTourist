71faa5b50818623d3716d91ac6a0d3037e76ce63
José Naves <josenaves@gmail.com>
2019-05-05 14:40:37 -0300

    feat: Add coredata model to project

---
+++ VirtualTourist/Model/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  VirtualTourist
+//
+//  Created by José Naves Moura Neto on 05/05/19.
+//  Copyright © 2019 José Naves Moura Neto. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
