09c9779da7ed568a1985195ae1c74e8f5bbfd060
José Naves <josenaves@gmail.com>
2019-05-05 16:42:36 -0300

    feat: Add detail screen

---
+++ VirtualTourist/Controller/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  VirtualTourist
+//
+//  Created by José Naves Moura Neto on 05/05/19.
+//  Copyright © 2019 José Naves Moura Neto. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
